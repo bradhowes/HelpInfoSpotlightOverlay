f1da06ecc136acf274b960c06cedd2b75e2142ba
Brad Howes <bradhowes@mac.com>
2026-04-25 12:43:10 +0200

    Drop package version requirement

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
   name: "HelpInfoSpotlightOverlay",
   platforms: [
-    .iOS(.v18)
+    .iOS(.v18), .macOS(.v15)
   ],
   products: [
     .library(
